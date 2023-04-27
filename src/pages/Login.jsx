ae3706aa24cbe91f20b829188ad2a86927fa337a
sueys <sueys@live.fr>
2023-04-27 15:49:05 +0200

    first commit add loging pages

---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Login.css"
+import { Link } from 'react-router-dom'
+
+const Login = () => {
+    return (
+        <div className='auth'>
+            <h1>Login</h1>
+            <form >
+                <input type="text" placeholder='username' />
+                <input type="password" placeholder='password' />
+                <button>login</button>
+                <span>Don't you have an account ? <Link to="/register">Register</Link></span>
+            </form>
+        </div>
+    )
+}
+
+export default Login
