c876bd626129a0c8d59781f2f9dd756d920e6386
sueys <sueys@live.fr>
2023-05-02 17:01:44 +0200

    add post edit

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Edit from '../img/edition.png'
+import Delete from '../img/supprimer.png'
+import { Link } from 'react-router-dom'
+import Menu from '../components/menu'
 
 const Single = () => {
     return (
-        <div>Single</div>
+        <div className='single'>
+            <div className="content">
+                <img src="https://picsum.photos/200/300?random=1" alt="" />
+                <div className='user'>
+                    <img src="https://picsum.photos/200/300?random=2" alt="" />
+                    <div className="info">
+                        <span>John</span>
+                        <p>Posted 2 days ago</p>
+                    </div>
+                    <div className='edit'>
+                        <Link to={`/write?edit=2`}>
+                            <img src={Edit} alt="" />
+                        </Link>
+                        {/* <link> */}
+                        <img src={Delete} alt="" />
+                        {/* </link> */}
+                    </div>
+                </div>
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, quisquam.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus tenetur rerum autem sapiente distinctio aliquid explicabo tempore nam molestiae atque.
+                    <br /><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat excepturi maxime officia placeat, reprehenderit modi illum minus asperiores ex, quam consequuntur, perferendis amet vero saepe. Quos quidem distinctio minima ratione, porro, totam impedit tempore sit cumque veniam officiis dolorem praesentium?</p>
+                    <br /><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo, ex? Voluptatum dolore beatae voluptatibus quibusdam aspernatur facere quisquam saepe laudantium tempore omnis delectus excepturi dolorum praesentium quod laborum consequuntur veniam iure fugiat libero, voluptatem, consequatur illo nostrum alias corrupti? Eveniet.</p>
+                </p>
+            </div>
+
+            <Menu />
+        </div>
     )
 }
 
--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const Write = () => {
+    const [value,setValue] = useState('');
+
+    console.log(value)
+
     return (
-        <div>Write</div>
+        <div className='add'>
+            <div className="content">
+                <input type="text" placeholder='Title' />
+                <div className="editorContainer"></div>
+                <ReactQuill theme="snow" value={value} onChange={setValue} />;
+            </div>
+            <div className="menu">
+                <div className="item">i1</div>
+                <div className="item">i2</div>
+            </div>
+        </div>
     )
 }
 
