e27a116e2894f49f000e9521f4f28a4afc693e51
sueys <sueys@live.fr>
2023-06-16 17:49:23 +0200

    commit

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Link } from 'react-router-dom'
 
 const Register = () => {
+    const [inputs, setInputs] = useState({
+        username:"",
+        email:"",
+        password:"",
+    })
+
+    const handleChange =e =>{
+
+    }
+
     return (
         <div className='auth'>
             <h1>Register</h1>
             <form >
-                <input required type="text" placeholder='username' />
-                <input required type="email" placeholder='email' />
-                <input required type="password" placeholder='password' />
+                <input required type="text" placeholder='username' onChange={handleChange} />
+                <input required type="email" placeholder='email' onChange={handleChange}/>
+                <input required type="password" placeholder='password' onChange={handleChange}/>
                 <button>register</button>
                 <p>This is an error !</p>
                 <span>Do you have an account ? <Link to="/login">Login</Link></span>
